aa33a81b35e40cf3d49ddb380e60715a86211476
Celeste Peláez <KernelFreeze@users.noreply.github.com>
2023-12-02 06:09:47 -0500

    Updated description

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import type { Metadata } from 'next'
 import { Ubuntu } from 'next/font/google'
 import { NextUIProvider } from './providers/next-ui'
 import { PropsWithChildren } from 'react'
+import { cn } from '@nextui-org/react'
 
 const ubuntu = Ubuntu({ weight: '400', subsets: ['latin'] })
 
@@ -13,7 +14,16 @@ export const metadata: Metadata = {
     authors: [{ name: 'Celeste Peláez', url: 'https://celestelove.dev' }],
     generator: 'CelesteServer',
     keywords: ['KernelFreeze', 'KernelUwU', 'CelesteLove', 'Portfolio'],
-    description: 'All the programming work done by CelesteLove.',
+    description: cn(
+        'Explore the digital realm of CelesteLove, a skilled developer with',
+        'a passion for crafting innovative and elegant solutions. Dive into a',
+        'showcase of her diverse portfolio, featuring web development projects',
+        'that blend creativity, functionality, and cutting-edge technology.',
+        "From responsive websites to dynamic applications, CelesteLove's portfolio",
+        'reflects a commitment to excellence and a love for pushing the boundaries',
+        'of digital possibilities. Discover the artistry in code and the transformative',
+        "power of technology at CelesteLove's portfolio website."
+    ),
 }
 
 export default function RootLayout({ children }: PropsWithChildren<{}>) {
