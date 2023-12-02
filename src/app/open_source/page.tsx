ec45464e1398aeef62641ffd41181960d75676b2
Celeste Peláez <KernelFreeze@users.noreply.github.com>
2023-12-02 13:30:19 -0500

    Added skeletons for the other pages

---
+++ src/app/open_source/page.tsx
@@ -0,0 +1,20 @@
+import Navbar from '@/app/components/navbar'
+import { ThemeRoot } from '@/app/components/theme'
+import dynamic from 'next/dynamic'
+
+const DynamicStarParticles = dynamic(
+    () => import('@/app/components/particles'),
+    {
+        ssr: false,
+    }
+)
+
+export default function Page() {
+    return (
+        <ThemeRoot>
+            <Navbar pageId='minecraft' />
+
+            <DynamicStarParticles />
+        </ThemeRoot>
+    )
+}
